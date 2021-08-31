13037c43c3c40cae5a244c3d2feb048526ddf6fa
DT <dvirto@mta.ac.il>
2021-08-31 14:07:11 +0300

    📝add faTimes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     FontAwesomeModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
